13564910eac305a8d23b0e744fa651261d35acb3
Tomas Garcia <tomasotano25@gmail.com>
2022-10-20 21:55:19 -0400

    initial commit

---
+++ components/container.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./navigation";
+
+const Container = (props) => {
+  return (
+    <div>
+      <Navigation />
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+export default Container;
